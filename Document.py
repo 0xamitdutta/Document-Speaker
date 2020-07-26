4c87d5f9f5e4aeaf8a05309b125d266517d73a2c
Amit Dutta <amitdutta2406@gmail.com>
2020-07-26 20:22:34 +0530

    Update Document.py

--- Document.py
+++ Document.py
@@ -1,5 +1,6 @@
 import os
 import PyPDF2
+import docx
 from gtts import gTTS
 import re
 from flask import Flask, render_template, request
@@ -7,7 +8,45 @@ from werkzeug import secure_filename
 app = Flask(__name__)
 
 
-def pdf_to_mp3(file_path):
+def getText(file_path):
+    if(os.path.exists(file_path)):
+        pass
+    else:
+        print("File does not exist")
+        exit()
+
+    doc = docx.Document(file_path)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+    return ('/n'.join(fullText))
+
+def getTextTxt(file_path):
+    if(os.path.exists(file_path)):
+        pass
+    else:
+        print("File does not exist")
+        exit()
+
+    f = open(file_path,"r")
+    return(f.read())
+
+def docsTxt(file_path):
+    string_words = getTextTxt(file_path)
+    print(string_words)
+    tts = gTTS(text=string_words, lang='en')
+    tts.save("C:/Users/ranjan/Desktop/listen_pdf.mp3")
+    os.system("mpg321 C:/Users/ranjan/Desktop/listen_pdf.mp3")
+
+def docs(file_path):
+    string_words = getText(file_path)
+    print(string_words)
+    tts = gTTS(text=string_words, lang='en', slow=True)
+    tts.save("C:/Users/ranjan/Desktop/listen_pdf.mp3")
+    os.system("mpg321 C:/Users/ranjan/Desktop/listen_pdf.mp3")
+
+def my_function(file_path):
+   
   if(os.path.exists(file_path)):
       pass
   else:
@@ -37,7 +76,6 @@ def pdf_to_mp3(file_path):
   tts.save("C:/Users/ranjan/Desktop/listen_pdf.mp3")
   os.system("mpg321 C:/Users/ranjan/Desktop/listen_pdf.mp3")
 
-
 @app.route('/')
 def upload_file():
   return '<html><body> <form action = "http://localhost:5000/upload" method = "POST" enctype = "multipart/form-data"><input type = "file" name = "file" /><input type = "submit"/></form></body></html>'
@@ -49,7 +87,11 @@ def upload_file1():
       f.save(secure_filename(f.filename))
       ext = f.filename.split('.')
       if ext[1] == "pdf":
-        pdf_to_mp3(request.files['file'].filename)
+        my_function(request.files['file'].filename)
+      elif ext[1] == "docx":
+        docs(f.filename)
+      elif ext[1] == "txt":
+        docsTxt(f.filename)
       else:
         print('Invalid File')
       return ('file generated successfully')
